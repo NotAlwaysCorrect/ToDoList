0dba6d59a3d524603c8fbf62b8e66b3347c60d41
Hua Chen Li <hua-chenli@hotmail.com>
2023-06-29 14:57:53 +1000

    My first little program, a super basic to do list I've only got the vaguest idea of how the swing GUI framework??? works. Youtube guy said don't make it pretty, just make it functional. The button deleting itself is funny. Maybe look at focus next time, so there is only one delete button? This was fun though. Frustrating at some parts, but rewarding at others. Syntax is still a nightmare though.

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Main implements ActionListener {
-
-    int count = 0;
-    JLabel label;
-//    JLabel taskListLabels;
+    JLabel label1, label2;
     JPanel panel;
     JFrame frame;
     JButton addTaskButton;
@@ -19,29 +16,8 @@ public class Main implements ActionListener {
     public Main() {
 
         frame = new JFrame();
-        addTaskButton = new JButton(new AbstractAction("Add Task") {
-            @Override
-            public void actionPerformed( ActionEvent e ) {
-                tempLabel = new JLabel(addTaskField.getText());
-                panel.add(tempLabel);
-                addTaskField.setText("");
-                JButton tempButton = new JButton("Delete");
-
-                panel.add(tempButton);
-
-                tempButton.addActionListener(e1 -> {
-                    panel.remove(panel.getComponentZOrder(tempButton) - 1);
-                    panel.remove(tempButton);
-                    panel.validate();
-                    panel.revalidate();
-                    panel.repaint();
-                });
-
-                panel.validate();
-
-            }
-        }
-        );
+        addTaskButton = new JButton("Add Task");
+        addTaskButton.addActionListener(this);
 
         addTaskField = new JTextField(30);
 
@@ -50,7 +26,11 @@ public class Main implements ActionListener {
         panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
         panel.setLayout(new GridLayout(0,2));
 
+        label1 = new JLabel("To Do List");
+        label2 = new JLabel("");
 
+        panel.add(label1);
+        panel.add(label2);
         panel.add(addTaskField);
         panel.add(addTaskButton);
 
@@ -59,15 +39,11 @@ public class Main implements ActionListener {
         frame.setTitle("To Do List");
         frame.pack();
         frame.setVisible(true);
-
-
-
-
     }
+
     public static void main(String[] args) {
         new Main();
     }
-
 
     @Override
     public void actionPerformed( ActionEvent e ) {
@@ -75,10 +51,12 @@ public class Main implements ActionListener {
         tempLabel = new JLabel(addTaskField.getText());
         panel.add(tempLabel);
         addTaskField.setText("");
+
         JButton tempButton = new JButton("Delete");
-
         panel.add(tempButton);
+        panel.validate();
 
+        System.out.println("here?");
         tempButton.addActionListener(e1 -> {
             panel.remove(panel.getComponentZOrder(tempButton) - 1);
             panel.remove(tempButton);
