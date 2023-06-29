06763751e60b102c4e2be9ee4c583a05577562ec
Hua Chen Li <hua-chenli@hotmail.com>
2023-06-29 14:32:07 +1000

    I got button to work. Now want to tidy it up

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
+import org.w3c.dom.ls.LSOutput;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main implements ActionListener {
 
     int count = 0;
@@ -12,49 +12,56 @@ public class Main implements ActionListener {
 //    JLabel taskListLabels;
     JPanel panel;
     JFrame frame;
-    JButton button;
     JButton addTaskButton;
     JTextField addTaskField;
-
+    JLabel tempLabel;
 
     public Main() {
 
         frame = new JFrame();
-        button = new JButton("Click me");
-//        taskListLabels = new JLabel();
         addTaskButton = new JButton(new AbstractAction("Add Task") {
             @Override
             public void actionPerformed( ActionEvent e ) {
-                panel.add(new JLabel(addTaskField.getText()));
-                panel.add(new JButton("Delete"));
+                tempLabel = new JLabel(addTaskField.getText());
+                panel.add(tempLabel);
+                addTaskField.setText("");
+                JButton tempButton = new JButton("Delete");
+
+                panel.add(tempButton);
+
+                tempButton.addActionListener(e1 -> {
+                    panel.remove(panel.getComponentZOrder(tempButton) - 1);
+                    panel.remove(tempButton);
+                    panel.validate();
+                    panel.revalidate();
+                    panel.repaint();
+                });
 
                 panel.validate();
 
             }
-        });
-
+        }
+        );
 
         addTaskField = new JTextField(30);
-        button.addActionListener(this);
 
         panel = new JPanel();
-        label = new JLabel("Number of clicks: ");
 
         panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
         panel.setLayout(new GridLayout(0,2));
-        panel.add(button);
-        panel.add(label);
 
 
         panel.add(addTaskField);
         panel.add(addTaskButton);
-//        panel.add(taskListLabels);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("To Do List");
         frame.pack();
         frame.setVisible(true);
+
+
+
 
     }
     public static void main(String[] args) {
@@ -63,9 +70,22 @@ public class Main implements ActionListener {
 
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        count++;
-        label.setText("Number of clicks: " + count);
+    public void actionPerformed( ActionEvent e ) {
 
+        tempLabel = new JLabel(addTaskField.getText());
+        panel.add(tempLabel);
+        addTaskField.setText("");
+        JButton tempButton = new JButton("Delete");
+
+        panel.add(tempButton);
+
+        tempButton.addActionListener(e1 -> {
+            panel.remove(panel.getComponentZOrder(tempButton) - 1);
+            panel.remove(tempButton);
+            panel.validate();
+            panel.revalidate();
+            panel.repaint();
+        }
+        );
     }
 }
